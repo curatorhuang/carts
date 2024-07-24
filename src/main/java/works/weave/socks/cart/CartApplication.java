72529762859b8416274e42b2118e75ed44de0127
curatorhuang <1011407982@qq.com>
2024-07-24 11:43:45 +0800

    1

---
+++ src/main/java/works/weave/socks/cart/CartApplication.java
@@ -0,0 +1,26 @@
+package works.weave.socks.cart;
+
+import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnablePrometheusEndpoint
+@SpringBootApplication
+@RestController
+public class CartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CartApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello, World!";
+    }
+}
